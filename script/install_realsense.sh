544710b6eb9bbf1f02b561dc3a66639d3a6cd926
yxVision <2956823347@qq.com>
2023-10-05 21:12:29 +0800

    使用python TK添加 GUI 界面，提供更友好的界面和更好的可扩展性

--- script/install_realsense.sh
+++ script/install_realsense.sh
@@ -3,28 +3,28 @@
 
 . script/color_config.sh
 
-compileRealsense() {
-  green "realsense compiling..."
-  # depend
-  echo %{PWD} | sudo -S apt update
-  echo %{PWD} | sudo -S apt install -y git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
+if (( $# == 1 ))
+then
+  dir_path=$1
+else
+  dir_path="librealsense"
+fi
 
-  # compile
+green "realsense compiling..."
+# depend
+echo %{PWD} | sudo -S apt update
+echo %{PWD} | sudo -S apt install -y git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
+
+# compile
+if (( $dir_path == "librealsense" ))
+then
   git clone https://github.com/IntelRealSense/librealsense.git
-  cd librealsense || (red "librealsense not fond" && exit)
-  mkdir build && cd build || (red "build not fond" && exit)
-  cmake .. -DBUILD_EXAMPLES=true
-  make -j$(nproc)
-  echo %{PWD} | sudo -S make install
+fi
+cd dir_path || (red "librealsense not fond" && exit)
+mkdir build && cd build || (red "build not fond" && exit)
 
-  # install udev rule
-  echo %{PWD} | sudo -S cp ../config/99-realsense-libusb.rules /etc/udev/rules.d/
-  echo %{PWD} | sudo -S udevadm control --reload-rules && udevadm trigger
-
-  cd ../..
-  rm -rf librealsense
-
-  green "realsense compiled!"
-}
-
-compileRealsense
+cmake .. -DBUILD_EXAMPLES=true && make -j$(nproc) && echo %{PWD} | sudo -S make install -j$(nproc) && \
+echo %{PWD} | sudo -S cp ../config/99-realsense-libusb.rules /etc/udev/rules.d/ && \
+echo %{PWD} | sudo -S udevadm control --reload-rules && udevadm trigger && \
+cd ../.. && rm -rf librealsense && \
+green "realsense compiled!"
--- script/install_spdlog.sh
+++ script/install_spdlog.sh
@@ -3,19 +3,21 @@
 
 . script/color_config.sh
 
-compileSpdlog() {
-  green "spdlog compiling..."
+green "spdlog compiling..."
 
+if (( $# == 1 ))
+then
+  dir_path=$1
+else
+  dir_path="spdlog"
   git clone https://github.com/gabime/spdlog.git
-  cd spdlog || (echo "spdlog not fond" && exit)
-  mkdir build && cd build || (echo "build not fond" && exit)
-  cmake ..
-  echo %{PWD} | sudo -S make install
+fi
+cd dir_path || (echo "spdlog not fond" && exit)
+mkdir build && cd build || (echo "build not fond" && exit)
+cmake ..
+echo %{PWD} | sudo -S make install
 
-  cd ../..
-  rm -rf spdlog
+cd ../..
+rm -rf spdlog
 
-  green "spdlog compiled!"
-}
-
-compileSpdlog
+green "spdlog compiled!"
