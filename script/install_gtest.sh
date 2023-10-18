112f48b0502c92493b1d310b0339e2c760782286
yxVision <2956823347@qq.com>
2023-10-18 19:41:07 +0800

    增加cmake下载、github下载函数， 增加fmt、openvino、glog、gtest、matplotlib-cpp、yaml-cpp

--- script/install_gtest.sh
+++ script/install_gtest.sh
@@ -3,4 +3,4 @@
 . script/color_config.sh
 . script/install_library_from_github.sh
 
-installLibraryFromGithub https://github.com/google/googletest.gitt
+installLibraryFromGithub https://github.com/google/googletest.git
