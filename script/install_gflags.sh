e4389263a8735e9205c49ac75f3b0bc12cce9be4
yxVision <2956823347@qq.com>
2023-11-03 20:59:58 +0800

    add gflags

---
+++ script/install_gflags.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+current_path=${PWD}
+cd /tmp && \
+git clone https://github.com/gflags/gflags.git --depth=1 && \
+cd gflags && \
+cmake -B build -G "Ninja" -DBUILD_SHARED_LIBS=ON && cmake --build build && sudo cmake --build build --target install
+cd current_path
