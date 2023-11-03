bc35f1476277e309457188b83837b63cc843e2ca
yxVision <2956823347@qq.com>
2023-11-03 20:27:22 +0800

    fix hik camera

--- script/install_hikcamera_x86.sh
+++ script/install_hikcamera_x86.sh
@@ -2,10 +2,11 @@
 
 . script/color_config.sh
 
-
+current_path=${PWD}
 cd "/tmp" && mkdir "hik_camera" && cd "hik_camera"
 wget -O "hik_camera.zip" "https://www.hikrobotics.com/cn2/source/support/software/MvCamCtrlSDK_STD_V4.1.2_231011.zip"
 unzip hik_camera.zip
 tar -xzvf MvCamCtrlSDK_Runtime-4.1.2_x86_64_20231011.tar.gz
 cd "MvCamCtrlSDK_Runtime-4.1.2_x86_64_20231011"
 sudo ./setup.sh
+cd current_path && blue "hik camera is Ok"
