add56253efd560496622d3d87b1bb5fae33b6edc
yxVision <2956823347@qq.com>
2023-11-03 20:32:11 +0800

    fix cmake-3.27.7

--- script/install_cmake_3.27.sh
+++ script/install_cmake_3.27.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget -O cmake-3.27.tar.gz https://github.com/Kitware/CMake/releases/download/v3.27.7/cmake-3.27.7.tar.gz
-tar -xzvf cmake-3.27
+tar -xzvf cmake-3.27.tar.gz
 cd cmake-3.27.7 && ./configure && make -j$(nproc) && echo %{PWD} | sudo -S make install -j$(nproc) && cd ../
