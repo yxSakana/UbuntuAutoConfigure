371f923884956cf80da0f54369198981b6ba5bf5
yxSakana <2956823347@qq.com>
2023-11-14 20:51:45 +0800

    fix hik camera

--- script/install_hikcamera_x86.sh
+++ script/install_hikcamera_x86.sh
@@ -4,9 +4,9 @@
 
 current_path=${PWD}
 cd "/tmp" && mkdir "hik_camera" && cd "hik_camera"
-wget -O "hik_camera.zip" "https://www.hikrobotics.com/cn2/source/support/software/MvCamCtrlSDK_STD_V4.1.2_231011.zip"
+wget -O "hik_camera.zip" "https://www.hikrobotics.com/cn2/source/support/software/MVS_STD_GML_V2.1.2_231011.zip"
 unzip hik_camera.zip
-tar -xzvf MvCamCtrlSDK_Runtime-4.1.2_x86_64_20231011.tar.gz
+tar -xzvf MVS-2.1.2_x86_64_20231011.tar.gz
 cd "MvCamCtrlSDK_Runtime-4.1.2_x86_64_20231011"
 echo %{PWD} | sudo -S ./setup.sh
 cd ${current_path} && blue "hik camera is Ok"
